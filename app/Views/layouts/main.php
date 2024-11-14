01a2470996b47039095fab4b5a58a8f9c34e9f60
sadiq <sadik.jsr@gmail.com>
2024-11-14 16:44:26 +0530

    footer added

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -12,9 +12,15 @@
 <body>
     <div class="container mt-5">	
         <?= $this->renderSection('content') ?>
+		<footer class="mt-2"><!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © <?php echo date('Y') ?> Copyright   
+  </div>
+  <!-- Copyright --></footer>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	
 </body>
 </html>
