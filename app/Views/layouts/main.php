fecf69caa6052f928373097e225dd5dd81b5ebb2
sadiq <sadik.jsr@gmail.com>
2024-11-14 16:46:21 +0530

    footer updated

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -13,10 +13,11 @@
     <div class="container mt-5">	
         <?= $this->renderSection('content') ?>
 		<footer class="mt-2"><!-- Copyright -->
-  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-    © <?php echo date('Y') ?> Copyright   
-  </div>
-  <!-- Copyright --></footer>
+		  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+			© <?php echo date('Y') ?> Copyright   
+		  </div>
+  <!-- Copyright -->
+		</footer>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
